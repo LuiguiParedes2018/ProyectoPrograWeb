6b04b9fde33b43b0b10df826df4d91c45ff622e0
JML037 <20192962@aloe.ulima.edu.pe>
2024-05-16 16:59:54 -0500

    JM-Masvendidos

--- src/componentes/Masvendidos.jsx
+++ src/componentes/Masvendidos.jsx
@@ -1,15 +1,43 @@
 import '../App.css'
+import React, { useEffect, useState } from 'react'
 import Menu from './Menu'
+import productos from './productos.json'
 import Pie from './Pie'
 
-function Masvendidos() {
-    return (
-        <>
-            <Menu />
-            <h1>AQUI ESTAN LOS PRODUCTOS MAS VENDIDOS</h1>
-            <Pie />
-        </>
-    )
-}
 
-export default Masvendidos
+
+const Masvendidos = () => {
+  const [productosMasVendidos, setProductosMasVendidos] = useState([]);
+
+  useEffect(() => {
+    const productosFiltrados = productos.filter(product => product.cantidadVendida > 0);
+    const productosOrdenados = productosFiltrados.sort((a, b) => b.cantidadVendida - a.cantidadVendida);
+    const productosMasVendidosDelMes = productosOrdenados.slice(0, 12);
+    setProductosMasVendidos(productosMasVendidosDelMes);
+  }, []);
+
+  return (
+    <>
+      <Menu />
+      <main>
+        <div>
+          <h1>Los 5 productos más vendidos del mes:</h1>
+        </div>
+        <ul className="product-list">
+          {productosMasVendidos.map((product, index) => (
+            <li key={index}>
+              <img src={product.imagen} alt={product.nombre} />
+              <div>
+                <h2>{product.nombre}</h2>
+                <span>${product.precio.toFixed(2)}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <Pie />
+    </>
+  );
+};
+
+export default Masvendidos;
