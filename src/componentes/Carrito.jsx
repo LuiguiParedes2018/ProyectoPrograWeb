bd99b836495a02d43a2510ba0d4d0867d6ab2ec1
Leonardo <20194424@alo.ulima.edu.pe>
2024-05-17 00:17:32 -0500

    Añadiendo lista de items en assets y funcion eliminar objetos, moviendo objetos

--- src/componentes/Carrito.jsx
+++ src/componentes/Carrito.jsx
@@ -1,12 +1,37 @@
 import '../App.css'
 import Menu from './Menu'
 import Pie from './Pie'
+import Objetos from "../assets/Lista.json";
+import ListaBoton from './ListaBoton'
+import React, { useState } from "react";
+
 
 function Carrito() {
+    const [productos, setProductos] = useState(Objetos);
+    const [productosSeleccionados, setProductosSeleccionados] = useState([]);
+
+    const eliminarProducto = (id) => {
+        const nuevaLista = productos.filter(producto => producto.id !== id);
+        setProductos(nuevaLista);
+    };
+
+
     return (
         <>
         <Menu/>
-            <h1>AQUI ESTAN TUS PRODUCTOS AÑADIDOS AL CARRITO</h1>
+        <div id='NumeroItems'>
+        <p><h2>Items en tu carrito de compras</h2></p>
+        </div>
+
+        <div className='Contenedor'>
+            <p>Items Disponibles para Envio:</p>
+            <ListaBoton/>
+
+            
+        </div>
+        <div className='Contenedor'>
+            <p>Guardado para despues:</p>
+        </div>
         <Pie/>
         </>
     )
---
+++ src/componentes/ListaBoton.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import Objetos from "../assets/Lista.json";
+
+function ListaBoton() {
+   const [productos, setProductos] = useState(Objetos);
+   const [productosSeleccionados, setProductosSeleccionados] = useState([]);
+
+
+    const eliminarProducto = (id) => {
+        const nuevaLista = productos.filter(producto => producto.id !== id);
+        setProductos(nuevaLista);
+    };
+
+    const paraDespues = (producto) => {
+     setProductosSeleccionados([...productosSeleccionados, producto]);
+       eliminarProducto(producto.id); 
+    };
+
+
+    return (
+        <div className='data'>
+            <ul >
+                {productos.map((data) => (
+                    <li key={data.id}>
+                        <img src={data.imagen} alt={data.nombre}/>
+                        <div id='valores'> 
+                        <p>{data.nombre}</p>
+                        <p>Precio: {data.precio}</p>
+     
+                        <div id= 'botones'>
+
+                        <p><button onClick={() => eliminarProducto(data.id)}>Eliminar</button></p>
+
+                        <p><button onClick={() => paraDespues(data)}>Guardar para despues</button></p>
+             
+             
+                        </div>
+                        </div>
+
+
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default ListaBoton;
