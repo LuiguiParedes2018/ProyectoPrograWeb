4db1f4998d6a98dce343153ab360361256c95d25
Nicolas0903 <144188823+Nicolas0903@users.noreply.github.com>
2024-05-17 20:11:38 -0500

    Update PaginaBusqueda.jsx

--- src/componentes/PaginaBusqueda.jsx
+++ src/componentes/PaginaBusqueda.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Menu';
 import Footer from './Pie';
 import ResultadosBusqueda from './ResultadosBusqueda';
-
+import "../Styles/PaginaBusqueda.css"
 const PaginaBusqueda = () => {
   return (
     <div>
